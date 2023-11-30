f6933d4aabe8a18da9640e74dbc4d0a7a3011c19
arslandeveloper1212 <arslanahmad7822@gmail.com>
2023-11-30 12:14:45 +0500

    add

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import LoginButton from './components/Login';
+import LogoutButton from './components/Logout';
+import Profile from './components/Profile';
+
+
+function App() {
+  return (
+    <div>
+    <LoginButton/>
+    <LogoutButton/>
+    <Profile/>
+
+    </div>
+  );
+}
+
+export default App;
